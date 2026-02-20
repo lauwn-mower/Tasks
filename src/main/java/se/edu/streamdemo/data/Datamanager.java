69662efa15d811163681d35585280f24d64b909a
wn <lauweina@gmail.com>
2026-02-20 17:07:54 +0800

    Fixed crime question mark

--- src/main/java/se/edu/streamdemo/data/Datamanager.java
+++ src/main/java/se/edu/streamdemo/data/Datamanager.java
@@ -60,7 +60,8 @@ public class Datamanager {
             ArrayList<String> dataItems = readFile();
             taskList = parse(dataItems);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
+            e.printStackTrace(); //this alone is a crime. you should at the very least do e.getMessage
         }
         return taskList;
     }
